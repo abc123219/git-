5af0d4e26713fc32a41dcec58fb7c0d76afd9b29
abc123219 <1070511173@qq.com>
2020-02-12 12:58:52 +0800

    二进制数转换成十进制数.practice

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <Windows.h>
 
+using namespace std;
+
 int main()
 {
 
-	std::cout << "Hool,wod;"<<std::endl;
+	char scn[32];
+	int a;
+	int k = 1;
+	int mun = 0;
+
+	cout << "请输入一个二进制数:";
+	cin >> scn;
+
+	a = strlen(scn);//计算数组值的个数
+
+
+
+	for (int i = a - 1; i >= 0; i--)
+	{
+		/*
+		1 2 4 8 16	二进制低位数到高位数 的十进制 值的 规律
+		1*k(1)=1;  k*2;  1*k(2)=2;  k*2;  1*k(4)=4;
+		*/
+
+		int x = scn[i] - '0';//将数组scn的值转换成整数类型
+
+		mun += x * k;//计算和
+
+		k *= 2;
+
+	}
+	printf("%d", mun);
+
 
 	system("pause");
 	return 0;
